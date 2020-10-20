35844d8d10955f0e7e9623dcd67459a6e03695e4
thisissandip <thisissandipmondal@gmail.com>
2020-10-20 11:25:15 +0530

    Updates

---
+++ precache-manifest.8028ed8b66d2d5ba0893eec6d15c4617.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989db4bb215add7480a92b1489f2493e",
+    "url": "/react-amazon-clone/index.html"
+  },
+  {
+    "revision": "f3cd9d1354826389a2f3",
+    "url": "/react-amazon-clone/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "2edff1ee4b1577beebae",
+    "url": "/react-amazon-clone/static/css/main.735d6aeb.chunk.css"
+  },
+  {
+    "revision": "f3cd9d1354826389a2f3",
+    "url": "/react-amazon-clone/static/js/2.82e1b86c.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/react-amazon-clone/static/js/2.82e1b86c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2edff1ee4b1577beebae",
+    "url": "/react-amazon-clone/static/js/main.4d1732c6.chunk.js"
+  },
+  {
+    "revision": "e509f85545c7d03ba483",
+    "url": "/react-amazon-clone/static/js/runtime-main.f321af18.js"
+  },
+  {
+    "revision": "600269652239060dc03bf481e4076a18",
+    "url": "/react-amazon-clone/static/media/header.60026965.jpg"
+  },
+  {
+    "revision": "f1b7fec6c618702caaa9a8aecf733608",
+    "url": "/react-amazon-clone/static/media/logo.f1b7fec6.png"
+  },
+  {
+    "revision": "8c04d548cacdc12600237ee9f9200b2a",
+    "url": "/react-amazon-clone/static/media/mobile header.8c04d548.jpg"
+  }
+]);
